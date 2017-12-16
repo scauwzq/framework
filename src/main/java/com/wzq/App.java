ac7aa9154c6f388a9069774e3891e394d1d1b0ad
scauwzq <985165870@qq.com>
2017-12-16 14:29:53 +0800

    增加memcache

--- src/main/java/com/wzq/App.java
+++ src/main/java/com/wzq/App.java
@@ -1,11 +1,13 @@
 package com.wzq;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletComponentScan;
 
 @SpringBootApplication
 @ServletComponentScan
+@MapperScan("com.wzq.dao")
 public class App {
   public static void main(String[] args) {
     SpringApplication.run(App.class, args);
--- src/main/java/com/wzq/controller/HelloController.java
+++ src/main/java/com/wzq/controller/HelloController.java
@@ -3,7 +3,9 @@ package com.wzq.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
+import com.whalin.MemCached.MemCachedClient;
 import com.wzq.model.User;
 import com.wzq.services.UserServices;
 
@@ -11,6 +13,15 @@ import com.wzq.services.UserServices;
 public class HelloController {
   @Autowired
   UserServices userServices;
+  @Autowired
+  MemCachedClient memCachedClient;
+
+  @RequestMapping("/cacheSave")
+  public String cacheSave(
+      @RequestParam(value = "id", required = false, defaultValue = "228") String id) {
+    boolean i = memCachedClient.set("id", id, 1000);
+    return String.valueOf(i);
+  }
   @RequestMapping("/")
   public String index() {
     Long id = 14l;
